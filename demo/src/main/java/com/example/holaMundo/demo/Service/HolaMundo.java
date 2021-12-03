20579cde2d920b41b73597f46c419d9391c42a13
Anibal Estuardo Muñoz Hernández <munoz4hernandez@gmail.com>
2021-12-03 17:31:32 -0600

    Hola Mundo en Spring

---
+++ demo/src/main/java/com/example/holaMundo/demo/Service/HolaMundo.java
@@ -0,0 +1,24 @@
+package com.example.holaMundo.demo.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service")
+@CrossOrigin
+public class HolaMundo {
+
+	
+	
+
+		@GetMapping("/holaMundo/{name}")
+		public String sayHello(@PathVariable("name")String name) {
+			return "Hola :" + name +" como estas?";
+		}
+	
+
+
+	
+}
